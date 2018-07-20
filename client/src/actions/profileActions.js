ec69471974175264acf3ed5af3bf0f856aa13370
Ian Liu <ianthl@outlook.com>
2018-07-20 20:01:33 +0800

    Can display profiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
@@ -75,6 +76,20 @@ export const deleteEducation = id => dispatch => {
       })
     )
     .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+// Get all profiles
+export const getProfiles = id => dispatch => {
+  dispatch(setProfileLoading);
+  axios
+    .get('/api/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err => dispatch({ type: GET_PROFILES, payload: null }));
 };
 
 // Delete profile & account
