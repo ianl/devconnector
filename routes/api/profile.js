0b7fad7e43c1995a7c6edcd3f84f80a95a87eafb
Ian Liu <ianthl@outlook.com>
2018-07-20 21:46:24 +0800

    Fix edit profile api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -117,25 +117,26 @@ router.post(
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.company) profileFields.company = req.body.company;
-    if (req.body.website) profileFields.website = req.body.website;
-    if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.status) profileFields.status = req.body.status;
-    if (req.body.bio) profileFields.bio = req.body.bio;
-    if (req.body.githubUsername)
-      profileFields.githubUsername = req.body.githubUsername;
+    profileFields.handle = req.body.handle;
+    profileFields.company = req.body.company;
+    profileFields.website = req.body.website;
+    profileFields.location = req.body.location;
+    profileFields.status = req.body.status;
+    profileFields.bio = req.body.bio;
+    profileFields.githubUsername = req.body.githubUsername;
+
     // Skills
     if (typeof req.body.skills !== 'undefined') {
       profileFields.skills = req.body.skills.split(',');
     }
+
     // Social
     profileFields.social = {};
-    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    profileFields.social.youtube = req.body.youtube;
+    profileFields.social.twitter = req.body.twitter;
+    profileFields.social.facebook = req.body.facebook;
+    profileFields.social.linkedin = req.body.linkedin;
+    profileFields.social.instagram = req.body.instagram;
 
     Profile.findOne({ user: req.user.id }).then(profile => {
       if (profile) {
