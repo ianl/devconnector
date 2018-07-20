0dbb2e15da7c4daff580e0de8e54abc1354c0e0a
Ian Liu <ianthl@outlook.com>
2018-07-20 16:29:35 +0800

    Able to delete profile & account

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from './types';
 
 // Get current profile
@@ -32,6 +33,26 @@ export const createProfile = (profileData, history) => dispatch => {
     .post('/api/profile', profileData)
     .then(res => history.push('/dashboard'))
     .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+// Delete profile & account
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This cannot be undone!')) {
+    axios
+      .delete('/api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
 
 // Profile loading
