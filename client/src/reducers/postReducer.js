7e7e65d9bb193154ee5c1bb31c517e2407668a8d
Ian Liu <ianthl@outlook.com>
2018-07-21 17:57:45 +0800

    Fix post insert order

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -22,7 +22,7 @@ export default (state = initialState, action) => {
     case ADD_POST:
       return {
         ...state,
-        posts: [...state.posts, action.payload]
+        posts: [action.payload, ...state.posts]
       };
     default:
       return state;
